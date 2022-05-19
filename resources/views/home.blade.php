ee12992cc545139c894c6840f8f173d4473c240d
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-19 19:40:11 +0200

    insert jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,37 @@
-@extends('layouts.app')
+@extends('layouts.index')
 
 
 @section('content')
+    <div class="jumbotron">
 
+        <div class="jumbotron-top">
+        </div>
 
-    @dump($comics)
+        <div class="container-jumbo">
+            <span class="title"> 
+                    CURRENT SERIES
+            </span>
+            @if(count($comics) > 0)
+                <div class="container70 d-flex-cards">
 
-    
+                    @foreach($comics as $item)
+                            <div class="card-rf">
+                                <a href="#">
+                                    <img src="{{ $item[ 'thumb' ] }}" alt="{{ $item[ 'series'] }}" >
+                                    <div>{{ $item[ 'series' ] }}</div>
+                                </a>
+                            </div>
+                    @endforeach
+                </div>
+            @endif
 
+            <div class="container70 d-flex-center pb-30">
+                <span class="text-box ">LOAD MORE</span>
+            </div>
+        </div>
 
+    </div>
 
 @endsection
+
+
