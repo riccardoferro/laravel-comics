233f050bf631816db090e3bb3881c0b6ccb79bf0
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-19 19:40:20 +0200

    little update

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,7 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/app.css">
+
+  <!-- BOOTSTRAP -->
+  <link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+  />
+
+  <link rel="stylesheet" href="/css/app.css">
   <title>DC comics </title>
 </head>
 <body>
@@ -12,9 +21,9 @@
     <!-- here we integrate the header -->
     @include('partials.header')
 
-
-    <!-- here we can extendes the content -->
     @yield('content')
+
+
 
 </body>
 </html>
