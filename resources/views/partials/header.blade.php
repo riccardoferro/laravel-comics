36304881f6139af8a3f51b0285d7620d6880df06
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-19 14:59:29 +0200

    insert partial header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+
+<header>
+  <nav class="container">
+
+    <!-- //logo -->
+    <div> <img src="images/dc-logo.png" alt="logo_dc_comics"> </div>
+
+    <div class="list-nav">
+
+      
+
+    </div>
+
+
+  </nav>
+</header>
