6bd519c742c307a105b3017331c6c62466509321
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-19 15:00:12 +0200

    showed data comics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   
+   $data = config('comics');
+
+
+   return view('home',["comics" => $data]);
 });
